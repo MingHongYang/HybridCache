6d1352c4a175f9e518e2e8a57ad08edf1295d4e8
Ming-Hong Yang <yang5445@umn.edu>
2019-10-24 21:21:42 -0500

    Add hash map in the list, do not maintain other maps when using this class

--- sim/linkedlist/LinkedList.h
+++ sim/linkedlist/LinkedList.h
@@ -5,6 +5,11 @@ struct Node {
         next = NULL;
     };
 
+    Node(PageInfo *newPage) : page(newPage) {
+        prev = NULL;
+        next = NULL;
+    };
+
     ~Node() {
         free(page);
     }
@@ -21,20 +26,23 @@ class LinkedList {
         // Destructor
         virtual ~LinkedList();
 
-        void addFront(Node &newNode);
-        void addBack(Node &newNode);
+        void addFront(Node *newNode);
+        void addBack(Node *newNode);
         bool removeTop();
         bool removeBottom();
         Node* getTop();
         Node* getBottom();
-        bool removeNode(Node &victim);
-        void removePage(Node &victim);
+        bool remove(uintmax_t pageNumber);
+        bool removeNode(Node *victim);
+        void removePage(Node *victim);
         uintmax_t getSize();
         void printList();
+        bool find(uintmax_t);
     private:
         Node *top;
         Node *bottom;
         uintmax_t size;
+        unordered_map<uintmax_t, Node *> mapping;
 };
 
 LinkedList::LinkedList() {
@@ -46,36 +54,42 @@ LinkedList::LinkedList() {
 LinkedList::~LinkedList() {
 }
 
-void LinkedList::addFront(Node &newNode) {
+void LinkedList::addFront(Node *newNode) {
     if (size == 0) {
         // Empty list, add the new one
-        top = &newNode;
-        bottom = &newNode;
+        top = newNode;
+        bottom = newNode;
     } else {
-        newNode.next = top;
-        top->prev = &newNode;
+        newNode->next = top;
+        top->prev = newNode;
         
-        top = &newNode;
+        top = newNode;
     }
 
     // Increase size
     size++;
+
+    // Update map
+    mapping.insert(make_pair(newNode->page->getPageNumber(), newNode));
 }
 
-void LinkedList::addBack(Node &newNode) {
+void LinkedList::addBack(Node *newNode) {
     if (size == 0) {
         // Empty list, add the new one
-        top = &newNode;
-        bottom = &newNode;
+        top = newNode;
+        bottom = newNode;
     } else {
-        newNode.prev = bottom;
-        bottom->next = &newNode;
+        newNode->prev = bottom;
+        bottom->next = newNode;
 
-        bottom = &newNode;
+        bottom = newNode;
     }
 
     // Increase size
     size++;
+
+    // Update map
+    mapping.insert(make_pair(newNode->page->getPageNumber(), newNode));
 }
 
 bool LinkedList::removeTop() {
@@ -85,12 +99,18 @@ bool LinkedList::removeTop() {
     } else if (size == 1) {
         free(top);
 
+        // Remove from map
+        mapping.clear();
+
         top = NULL;
         bottom = NULL;
     } else {
         Node *temp = top;
         top = top->next;
         top->prev = NULL;
+
+        // Remove from map
+        mapping.erase(temp->page->getPageNumber());
 
         free(temp);
     }
@@ -108,12 +128,18 @@ bool LinkedList::removeBottom() {
     } else if (size == 1) {
         free(bottom);
 
+        // Remove from map
+        mapping.clear();
+
         top = NULL;
         bottom = NULL;
     } else {
         Node *temp = bottom;
         bottom = bottom->prev;
         bottom->next = NULL;
+
+        // Remove from map
+        mapping.erase(temp->page->getPageNumber());
 
         free(temp);
     }
@@ -132,29 +158,42 @@ Node* LinkedList::getBottom() {
     return bottom;
 }
 
-bool LinkedList::removeNode(Node &victim) {
-    if (victim.prev != NULL) {
-        victim.prev->next = victim.next;
+bool LinkedList::remove(uintmax_t pageNumber) {
+    if (mapping.count(pageNumber)) {
+        removeNode(mapping[pageNumber]);
+
+        return true;
+    }
+
+    return false;
+}
+
+bool LinkedList::removeNode(Node *victim) {
+    if (victim->prev != NULL) {
+        victim->prev->next = victim->next;
     } else {
         // This is top
-        top = victim.next;
+        top = victim->next;
     }
 
-    if (victim.next != NULL) {
-        victim.next->prev = victim.prev;
+    if (victim->next != NULL) {
+        victim->next->prev = victim->prev;
     } else {
         // This is bottom
-        bottom = victim.prev;
+        bottom = victim->prev;
     }
 
-    free(&victim);
+    // Remove from map
+    mapping.erase(victim->page->getPageNumber());
+
+    free(victim);
 
     // Decrease size
     size--;
 }
 
-void LinkedList::removePage(Node &victim) {
-    free(victim.page);
+void LinkedList::removePage(Node *victim) {
+    free(victim->page);
 }
 
 uintmax_t LinkedList::getSize() {
@@ -170,5 +209,13 @@ void LinkedList::printList() {
     }
 
     cout << endl;
+}
+
+bool LinkedList::find(uintmax_t pageNumber) {
+    if (mapping.count(pageNumber)) {
+        return true;
+    }
+
+    return false;
 }
 
